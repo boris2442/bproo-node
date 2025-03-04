bdcc231e8611818966419d2ed6f7780f1f48a4be
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-04 14:54:52 +0100

    test

--- lecon6/server.js
+++ lecon6/server.js
@@ -2,18 +2,18 @@ const http=require('http');
 const fs=require('fs');
 const server=http.createServer((req, res)=>{
     // console.log("nous venons de recevoir une requette");
-    console.log(req.url, req.method)
-    res.setHeader('content', 'text/html');
+    // console.log(req.url, req.method)
+    res.setHeader('content', 'text/html' );
     // res.write("<h1>Salut tout le monde</h1>");
-    fs.readFile("./view/index.html", (err, data)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.write(data);
-            res.end()
-        }
-    })
-    res.end();
+    // fs.readFile("./view/index.html", (err, data)=>{
+    //     if(err){
+    //         console.log(err)
+    //     }else{
+        
+    //         res.end(data)
+    //     }
+    // })
+   
 })
 
 server.listen(3000, ()=>{
